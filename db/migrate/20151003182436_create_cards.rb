11856efb5486c07c2445e6e43115e93bb7d466e0
Christian Neal <c.neal91@gmail.com>
2015-10-03 13:28:45 -0500

    Set up data model with games, hands, and cards

---
+++ db/migrate/20151003182436_create_cards.rb
@@ -0,0 +1,13 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.string :suit
+      t.integer :value
+      t.string :name
+      t.integer :game_id
+      t.integer :hand_id
+
+      t.timestamps null: false
+    end
+  end
+end
